a2835a606fe55e730f78c2939b3fb0a25b0d9770
yibon <yibonnn@gmail.com>
2023-08-31 16:36:59 +0800

    Fishing thing to boop boop

--- My project/Assets/Scripts/Fishing.cs
+++ My project/Assets/Scripts/Fishing.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Fishing : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform topPivot;
+    [SerializeField] Transform bottomPivot;
+
+    [SerializeField] Transform fish;
+
+    float fishPos;
+    float fishDestination;
+
+    float fishTimer;
+    [SerializeField] float timerMultiplicator = 3f;
+
+    float fishSpeed;
+    [SerializeField] float smoothMotion = 1f;
+
+    bool isFishing;
+
+    private void Start()
     {
-        
+        fishSpeed = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isFishing)
+        {
+            Debug.Log("hello");
+            fishTimer = Random.value * timerMultiplicator;
+            fishDestination = Random.value;
+        }
+
+        fishPos = Mathf.SmoothDamp(fishPos, fishDestination, ref fishSpeed, smoothMotion);
+        fish.position = Vector3.Lerp(bottomPivot.position, topPivot.position, fishPos);
+    }
+
+    private void FixedUpdate()
+    {
+        fishTimer -= Time.deltaTime;
+        if (fishTimer < 0f)
+        {
+            isFishing = false;
+        }
     }
 }
