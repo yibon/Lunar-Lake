a3c04852990deabcae0e63dcc98398cd14b4208b
yibon <90676968+yibon@users.noreply.github.com>
2023-09-13 02:21:10 +0800

    FUCK YEAH  BABY

--- My project/Assets/Scripts/Line.cs
+++ My project/Assets/Scripts/Line.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Line : MonoBehaviour
@@ -14,20 +15,42 @@ public class Line : MonoBehaviour
     public float targetDist;
     public float smoothSpeed;
 
+    [SerializeField] private Transform initPos;
+    [SerializeField] private Transform targetPos;
+    [SerializeField] private Transform interpolatePt;
+
+    private float interpolateAmt;
+    [SerializeField] Transform flowPoint;
+
+    bool isCasting;
+    bool temp = false;
+
+    int flag = 0;
+
     private void Start()
     {
         lineRend.positionCount = length;
         segmentPoses = new Vector3[length];
         segmentV = new Vector3[length];
-        targetDir.position = new Vector3(-4f, 1.6f, 0);
 
-        StartCoroutine(Casting());
+        //initPos = new Vector3(0f, 1.6f, 0);
+        //interpolatePt = new Vector3(-8f, 4f, 0); 
+        //targetPos = new Vector3 (-6f, 4.5f, 0);
+
+        targetDir.position = initPos.position;
+
+        //StartCoroutine(Casting());
 
     }
 
     private void Update()
     {
-        //segmentPoses[0] = targetDir.position;
+        segmentPoses[0] = targetDir.position;
+
+        if (Input.GetMouseButton(0))
+        {
+            isCasting = true;
+        }
 
         for (int i = 1; i < segmentPoses.Length; i++)
         {
@@ -37,62 +60,73 @@ public class Line : MonoBehaviour
         lineRend.SetPositions(segmentPoses);
     }
 
-    private IEnumerator Casting()
+    private void FixedUpdate()
     {
-        Debug.Log(targetDir.position);
-
-        // Start Casting
-        Debug.Log("Start Casting");
-        segmentPoses[0] = targetDir.position;
-        yield return new WaitForSeconds(0.5f);
-
-        // Pull back the rod
-        Debug.Log("Pull Back Rod");
-
-        Vector3 interval = new Vector3(-0.05f, 0.02f, 0);
-        Vector3 halfwayPos = new Vector3(-6f, 2.4f, 0);
-        Vector3 targetPos = new Vector3(-8.00f, 1.60f, 0);
-
-        while (targetDir.position != halfwayPos)
+        if (isCasting)
         {
-            targetDir.position += interval;
-            Debug.Log(targetDir.position);
-            yield return null;
-            segmentPoses[0] = targetDir.position;
+            //Debug.Log(interpolateAmt);
+            interpolateAmt = (interpolateAmt + Time.deltaTime);
+            Vector3 pointAB;
+            Vector3 pointBC;
+
+            if (targetDir.position == targetPos.position)
+            {
+                temp = true;
+                // This value CANNOT be equals to zero
+                interpolateAmt = 0.01f;
+            }
+
+
+            if (temp)
+            {
+                //interpolateAmt = 0;
+                pointAB = Vector3.Lerp(targetPos.position, interpolatePt.position, interpolateAmt);
+                pointBC = Vector3.Lerp(interpolatePt.position, initPos.position, interpolateAmt);
+
+                targetDir.position = Vector3.Lerp(pointAB, pointBC, interpolateAmt);
+                flowPoint.position = Vector3.Lerp(pointAB, pointBC, interpolateAmt);
+                //isCasting = false;
+            }
+
+            else
+            {
+                pointAB = Vector3.Lerp(initPos.position, interpolatePt.position, interpolateAmt);
+                pointBC = Vector3.Lerp(interpolatePt.position, targetPos.position, interpolateAmt);
+
+                targetDir.position = Vector3.Lerp(pointAB, pointBC, interpolateAmt);
+                flowPoint.position = Vector3.Lerp(pointAB, pointBC, interpolateAmt);
+            }
         }
-
-        //interval = new Vector3(-0.05f, -0.02f, 0);
-
-        //while (targetDir.position != targetPos)
-        //{
-        //    targetDir.position += interval;
-        //    Debug.Log(targetDir.position + "||" + targetPos);
-        //    yield return null;
-        //    segmentPoses[0] = targetDir.position;
-        //}
-
-        yield return new WaitForSeconds(0.5f);
-
-
-        // Launch the rod
-        Debug.Log("Launch");
-
-        interval = new Vector3(0.05f, -0.02f, 0);
-        halfwayPos = new Vector3(-4f, 1.6f, 0);
-
-        while (targetDir.position != halfwayPos)
-        {
-            targetDir.position += interval;
-            Debug.Log(targetDir.position);
-            yield return null;
-            segmentPoses[0] = targetDir.position;
-        }
-
-
-
-        segmentPoses[0] = targetDir.position;
-        yield return new WaitForSeconds(1f);
-
     }
 
-}
+        //private IEnumerator Casting()
+        //{
+        //    //Debug.Log(targetDir.position);
+        //    isCasting = false;
+
+        //    // Start Casting
+        //    Debug.Log("Start Casting");
+        //    segmentPoses[0] = targetDir.position;
+        //    yield return new WaitForSeconds(1f);
+
+        //    // Pull back the rod
+        //    Debug.Log("Pull Back Rod");
+        //    yield return new WaitForSeconds(3f);
+
+
+        //    // Launch the rod
+        //    //Debug.Log("Launch");
+        //    //targetDir.position = QuadraticLerp(targetPos, interpolatePt, initPos, interpolateAmt);
+        //    //Debug.Log(targetDir.position);
+
+        //}
+
+        //private Vector3 QuadraticLerp(Vector3 a, Vector3 b, Vector3  c, float t)
+        //{
+        //    Vector3 ab = Vector3.Lerp(a, b, t);
+        //    Vector3 bc = Vector3.Lerp(b, c, t);
+
+        //    return Vector3.Lerp(ab, bc, interpolateAmt);
+        //}
+
+    }
