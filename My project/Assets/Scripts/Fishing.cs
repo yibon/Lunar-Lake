ea4e6ceaa634906e97dd9126a27622d2d472400c
yibon <90676968+yibon@users.noreply.github.com>
2023-09-19 21:16:26 +0800

    delete stuff for build

--- My project/Assets/Scripts/Fishing.cs
+++ My project/Assets/Scripts/Fishing.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class Fishing : MonoBehaviour
--- My project/Assets/Scripts/Line.cs
+++ My project/Assets/Scripts/Line.cs
@@ -1,8 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using Unity.VisualScripting;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class Line : MonoBehaviour
