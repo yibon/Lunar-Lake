eeb08284b32ac0ad08dcdec225f49a7b9175de05
PlainCivilian <2200929@sit.singaporetech.edu.sg>
2023-10-18 10:04:50 +0800

    edited fish script will edit other also

---
+++ My project/Assets/Scripts/Data/Events.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Events
+{
+    #region declaring information
+
+    public string eventID;
+    public string eventName;
+    public string eventClearCondiditon;
+    public string eventRewards;
+    public string eventEffects;
+    public string occurence;
+    public bool isDone;
+    #endregion
+}
