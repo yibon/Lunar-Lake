abac7311143af357654a2b0e1a452abfcdd9ede7
PlainCivilian <2200929@sit.singaporetech.edu.sg>
2023-10-18 10:12:44 +0800

    edited event and rods cs for better use

--- My project/Assets/Scripts/Data/Events.cs
+++ My project/Assets/Scripts/Data/Events.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Events
 {
-    #region declaring information
 
-    public string eventID;
-    public string eventName;
-    public string eventClearCondiditon;
-    public string eventRewards;
-    public string eventEffects;
-    public string occurence;
-    public bool isDone;
-    #endregion
+    public string eventID { get; set; }
+    public string eventName { get; set; }
+    public string eventClearCondiditon { get; set; }
+    public string eventRewards { get; set; }
+    public string eventEffects { get; set; }
+    public string occurence { get; set; }
+    public bool isDone { get; set; }
+
+
+    public Events( string eventID, string eventName, string eventClearCondition, string eventRewards, string eventEffects, string occurence, bool isDone )
+    {
+        this.eventID = eventID;
+        this.eventName = eventName;
+        this.eventClearCondiditon = eventClearCondition;
+        this.eventRewards = eventRewards;
+        this.eventEffects = eventEffects;
+        this.occurence = occurence;
+        this.isDone = isDone;
+
+    }
 }
